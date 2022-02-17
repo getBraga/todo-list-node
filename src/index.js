21063aa617f51cf5f6e0100c5b6a3ca73382f1a0
getBraga <37493037+getBraga@users.noreply.github.com>
2022-02-17 11:42:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,41 @@ const handler = (request, response) => {
   return chosen(request, response)
 }
 
-http.createServer(handler).listen((PORT), () => {
+const server =  http.createServer(handler).listen((PORT), () => {
   
   console.log('service running at', PORT)
 })
+
+
+server.on("request", function (request, response) {
+	console.log(`METHOD: ${request.method}; URL: ${request.url}`);
+	switch(request.method)
+	{
+	case "GET":
+	case "PUT":
+	case "POST":
+	case "PATCH":
+	case "DELETE":
+		response.writeHead(200, {
+			"Content-Type":"application/json",
+			"Access-Control-Allow-Origin":"*", // REQUIRED CORS HEADER
+			"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept" // REQUIRED CORS HEADER
+		});
+		response.end(JSON.stringify({dummy:"dummy"}));
+		break;
+	case "OPTIONS": // THE CLIENT OCCASIONALLY - NOT ALWAYS - CHECKS THIS
+		response.writeHead(200, {
+			"Access-Control-Allow-Origin":"*", // REQUIRED CORS HEADER
+			"Access-Control-Allow-Methods":"GET, POST, DELETE, PUT, PATCH", // REQUIRED CORS HEADER
+			"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept" // REQUIRED CORS HEADER
+		});
+		response.end();
+		break;
+	default:
+		response.writeHead(405, {
+			"Content-Type":"application/json"
+		});
+		response.end(JSON.stringify({error:`method ${request.method} not allowed`}));
+		break;
+	}
+});
