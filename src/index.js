93ea95904ef16b4753a3732083f6313ae857764f
getBraga <37493037+getBraga@users.noreply.github.com>
2022-02-17 11:39:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,6 @@ const { routes } = require('./../src/routes/Todo')
 const PORT = process.env.PORT || 4000
 
 const handler = (request, response) => {
-  const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-      "Access-Control-Max-Age": 2592000, // 30 days
-      /** add other headers as per requirement */
-    };
-
-    if (request.method === "OPTIONS") {
-      request.writeHead(204, headers);
-      response.end();
-      return;
-    }
-
-    if (["GET", "POST"].indexOf(request.method) > -1) {
-      response.writeHead(200, headers);
-      response.end("Hello World");
-      return;
-    }
-
-    response.writeHead(405, headers);
-    response.end(`${request.method} is not allowed for the request.`);
   const { url, method } = request
   let routeKey = `${method.toLowerCase()}:${url}`
   const [,, id] = routeKey.split('/')
